9260a8afae4bf99d8002039bd161a7e97e15adab
Syed Mustafa Hassan <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2024-05-13 01:14:20 +0500

    Create l3.js

---
+++ l3.js
@@ -0,0 +1,32 @@
+let person = "Hege";
+let person="Hege";
+let x = y + z;
+
+document.getElementById("demo").innerHTML = "Hello Dolly!";
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dolly!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+let x, y;
+x = 5;
+y = 6;
+
+
+let x, y;
+x = 5 + 6;
+y = x * 10;
+
+var x, y;
+x = 5 + 6;
+y = x * 10;
